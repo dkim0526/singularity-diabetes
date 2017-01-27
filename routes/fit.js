ad9aebacf2daee42f2cf479ceb0398ac56db90f3
Shivam Sharma <sdsharma@ucsd.edu>
2017-01-27 15:45:21 -0800

    pull info from current day

--- routes/fit.js
+++ routes/fit.js
@@ -7,7 +7,21 @@ var client = new FitbitApiClient("227H7Z", "d9a1cd45ceb53e96d4b9eeac77af5c60");
 const API_BASE_PATH = '/api/fit';
 
 const routes = [];
+function currentDate(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10) {
+        dd='0'+dd;
+    } 
 
+    if(mm<10) {
+        mm='0'+mm;
+    } 
+    today = yyyy+'-'+mm+'-'+dd;
+    return today;
+}
 // GET /api/fit
 routes.push({
     method: 'GET',
@@ -57,9 +71,9 @@ routes.push({
     config: {
         auth: false,
         handler: function (request, reply) {
-            console.log(request.query.code);
             client.getAccessToken(request.query.code, 'http://localhost:3000/api/fit/callback').then(function (result) {
-                client.get("/profile.json", result.access_token).then(function (results) {
+                var date = currentDate();
+                client.get("/activities/date/" + date + ".json", result.access_token).then(function (results) {
                     reply(results[0]);
                 });
             }).catch(function (error) {
