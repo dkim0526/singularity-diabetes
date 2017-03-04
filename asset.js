33686932bdddd781954a637968c36b859d5560ed
Daniel Kim <bjkim0526@gmail.com>
2017-03-04 00:14:57 -0800

    added front assets and worked on stuff

---
+++ asset.js
@@ -0,0 +1,34 @@
+
+
+
+console.log("12345");
+
+$(document).ready(function(){
+	$("#startBtn").click(function(){
+		console.log("whatip");		
+		$(".circle").flip({
+    		front: '.front',
+    		axis: 'x'
+		});
+	});
+
+	
+
+
+
+	
+
+// $('.front').transition({
+//   perspective: '100px',
+//   rotate3d: '45,45,0,180deg'
+// });
+
+
+	$("#nextBtn").click(function(){
+	    if( $("#nameField").val().length > 0){
+	    	Cookies.set('userName', $("#nameField").val());
+	    	$(".front").empty();
+
+	    }
+	});
+});
